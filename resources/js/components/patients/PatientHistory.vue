45df66bc63d6a541c6f581bee412b96d398a74b0
quanghm <quanghm27@gmail.com>
2020-06-13 23:18:28 +0700

    update time get history

--- resources/js/components/patients/PatientHistory.vue
+++ resources/js/components/patients/PatientHistory.vue
@@ -51,7 +51,7 @@
             async inital() {
                 let params = [{
                     'sharedKey' : this.shareKey,
-                    'fromDate': moment().subtract(5, 'days'),
+                    'fromDate': moment().subtract(10, 'days'),
                     'toDate': moment()
                 }]
                 let result = await axios.post(API_HISTORY, params)
