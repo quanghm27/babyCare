36456c2bd31ee07cf6ef829b05cbff4c1a30433c
quanghm <quanghm27@gmail.com>
2020-06-19 23:02:09 +0700

    update UI

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,7 +13,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <link rel="stylesheet" href="/css/app.css">
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini sidebar-collapse">
 <div id="app" class="wrapper">
 
     @include('layouts.navbar')
